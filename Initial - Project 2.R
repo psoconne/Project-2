02ee47e4fefce52b1ccc1fb61770429542902c57
Paige O'Connell <psoconne@ncsu.edu>
2024-07-06 16:35:48 -0400

    Second commit: added code for numerical summaries and plots

--- Initial - Project 2.R
+++ Initial - Project 2.R
@@ -1,6 +1,7 @@
 library(httr)
 library(jsonlite)
-library(tibble)
+library(tidyverse)
+
 
 MarketAPI <- function(symbol, type, date_from=NULL, date_to=NULL) {
   #url <- paste0("https://newsapi.org/v2/everything?q=", subject, "&from=", date, "&apiKey=", api_key)
@@ -33,16 +34,40 @@ MarketAPI <- function(symbol, type, date_from=NULL, date_to=NULL) {
   return(market_info)
 }
 
-MarketAPI("AAPL", type = "eod")
+AAPL_data <- MarketAPI("AAPL", type = "intraday")
 
 
 
-#################################
-url <- paste0("http://api.marketstack.com/v1/eod", 
-              "?access_key=d4f48f4fa8307e7e0163fc92cce500d2", 
-              "&symbols=", "AAPL",
-              "&date_from=", "2024-06-26",
-              "&date_to=", "2024-07-06")
-market_data <- httr::GET(url)
-market_parsed <- jsonlite::fromJSON(rawToChar(market_data$content))
-market_info <- tibble::as_tibble(market_parsed$data)
+# Contingency table for grouped open prices
+df <- AAPL_data %>%
+  mutate(open_group = cut(open, breaks = seq(160, max(open, na.rm = TRUE) + 10, by = 10), right = FALSE))
+table(df$open_group)
+
+# Numerical Summaries of open and close prices
+numerical_summaries <- df %>%
+  summarise(across(c(open, close), list(mean = mean, sd = sd), na.rm = TRUE))
+print(numerical_summaries)
+
+
+
+# Plot 1 : closing price over time
+ggplot(df, aes(x = as.Date(date), y = close, group = 1)) +
+  geom_line(color = "red") +
+  labs(title = "Closing Prices Over Time", x = "Date", y = "Closing Price")
+
+# Plot 2 : opening prices over time
+ggplot(df, aes(x = as.Date(date), y = open, group = 1)) +
+  geom_line(color = "red") +
+  labs(title = "Opening Prices Over Time", x = "Date", y = "Opening Price")
+
+# Plot 3: Scatter plot of high vs low prices
+ggplot(df, aes(x = open, y = close)) +
+  geom_point(color = "blue") +
+  labs(title = "High vs Low Prices", x = "Low Price", y = "High Price")
+
+# Plot 4: Density plot of open prices
+ggplot(df, aes(x = open, fill = open_group)) +
+  geom_density(alpha = 0.5) +
+  labs(title = "Density Plot of Opening Prices by Open Group", x = "Opening Price", y = "Density", fill = "Open Group")
+
+
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+---
+editor_options: 
+  markdown: 
+    wrap: 72
+---
+
 # Project-2
+
+Brief description of the app and its purpose.
+
+-   A list of packages needed to run the app:
+
+    -   `httr`, `jsonlite`, `tidyverse`, `shiny`
+
+-   A line of code that would install all the packages used (so we can
+    easily grab that and run it prior to running your app).
+
+    -   `library()`
+
+-   The shiny::runGitHub() code that we can copy and paste into RStudio
+    to run your app.
+
+    -   ....
